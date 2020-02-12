e6f190699b6c88529305e36213ce7862f02cc8dd
Reunji <55166669+Reunji@users.noreply.github.com>
2020-02-11 21:20:10 -0500

    update

--- Python Homework.py
+++ Python Homework.py
@@ -6,15 +6,16 @@ import random
 # welcoming the user
 
 
-print("Press any character to see your cool nickname")
+print("Press enter to see your codename")
 input()
 # wait for 1 second
 time.sleep(1)
 
 
-firstName = '''Murderous Wicked The Powerful abrupt bad corrupt destructive hateful heinous hideous malevolent malicious nefarious ugly unpleasant vicious
-vile villainous wicked base foul low offensive poison reprobate wrong angry atrocious baneful beastly calamitous damnable
-depraved disastrous execrable harmful iniquitous injurious loathsome maleficent malignant obscene pernicious rancorous repugnant repulsive revolting spiteful stinking unpropitious wrathful '''
+firstName = '''Murderous Wicked The Powerful abrupt bad corrupt destructive hateful heinous hideous malevolent malicious  
+unpleasant vicious vile villainous wicked foul low offensive poison reprobate wrong angry atrocious baneful beastly 
+depraved disastrous execrable harmful iniquitous injurious loathsome maleficent malignant obscene pernicious rancorous
+repugnant repulsive revolting spiteful stinking unpropitious wrathful calamitous damnable nefarious ugly'''
 
 lastName = '''Python Hawk Falcon Anaconda Viper Abomination 
 Captain Baboon Ape Tiger Shark King Communist'''
@@ -29,6 +30,13 @@ word2 = random.choice(lastName)
 # creates an variable with an empty value
 firstName = ''
 lastName = ''
-print("Hello" + " " + word1 + " " + word2 + ".")
+word1 = word1.capitalize()
+word2 = word2.capitalize()
+print("Hello" + " " + word1 + " " + word2 + ".\n")
+exitMessage = "This message will disappear in 3 seconds"
+exitMessage = exitMessage.capitalize()
+print(exitMessage)
+time.sleep(2.5)
+quit()
 
 
